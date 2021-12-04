3e4e306cbcc486d04a8cf7d73f7c1044dec1113f
Eduardo <eduardoguette@gmail.com>
2021-12-04 22:57:16 +0100

    refactorización de codigo

--- Reto 4/solucion-reto-4.js
+++ Reto 4/solucion-reto-4.js
@@ -1,10 +1,14 @@
-export default function createXmasTree(height) {
-  let arbol = '';
+function createXmasTree(height) {
+  let arbol = ''; 
   for (let i = 1; i <= height; i++) {
-    arbol += `${'_'.repeat(height - i) + '*'.repeat(i * 2 - 1) + '_'.repeat(height - i)}\n`;
-    if(i === height){
-      arbol += `${'_'.repeat(height - 1) + '#' + '_'.repeat(height - 1)}\n`.repeat(2)
+    const space = '_'.repeat(height - i);
+    const space_bottom = '_'.repeat(height - 1);
+    const ramas = '*'.repeat(i * 2 - 1);
+    arbol += `${space + ramas + space}\n`;
+    if (i === height) {
+      arbol += `${space_bottom + '#' + space_bottom}\n`.repeat(2);
     }
   }
   return arbol.trim();
 }
+console.log(createXmasTree(5))
